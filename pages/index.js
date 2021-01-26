b44afa563de307791df96f95335049d743fdc6ca
andermsilva <andermarsil@gmail.com>
2021-01-25 22:19:10 -0300

    initial-commit primeira aula imerção react-nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,65 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import  GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+/* const BackgroundImage = styled.div`
+   background-image:url(${db.bg});
+   flex:1;
+   background-size: cover;
+   background-position:center;
+`;
+ */
+
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+   
+       <QuizBackground backgroundImage={db.bg} >
+          <QuizContainer>
+            <QuizLogo />
+            <Widget>
+              <Widget.Header>
+                    <h1> Astronomia </h1>
+              </Widget.Header>
+               <Widget.Content>
+                 <p> lourem dolor ipc .....</p>
+               </Widget.Content>
+            </Widget>
+
+            <Widget>
+               <Widget.Content>
+              <h1> O que voce sabe sobre o universo? </h1>
+              <p> lourem dolor ipc .....</p>
+
+               </Widget.Content>
+            </Widget>
+         
+          <Footer/>
+          
+          </QuizContainer>  
+          <GitHubCorner projectUrl='https://github.com/omariosouto'/>
+       </QuizBackground>
+
+      
+
+  ) 
 }
